f6af1f57908c32066a7ed72e40aa40f37eacf2b4
ankitrathore31 <ankitrathore3110@gmail.com>
2025-07-02 18:35:57 +0530

    Updates

--- resources/views/Home/pages/project.blade.php
+++ resources/views/Home/pages/project.blade.php
@@ -1,15 +1,22 @@
 @extends('Home.layout.master')
-
 @section('content')
     {{-- <div class="container"> --}}
-        <div class="main-content">
-            <!-- Home section start  -->
-            <section class="home active section " id="home">
-                <div class="container">
-                    <div class="row">
-                  
+    <div class="main-content">
+        <!-- Home section start  -->
+        <section class="home active section " id="home">
+            <div class="container">
+                <div class="row">
+                    <div class="section-title padd-15">
+                        <h2>Projects</h2>
+                    </div>
                 </div>
-            </section>
-        </div>
+                <div class="row">
+                    <div class="certificate-heading padd-15">
+                        <h2>My Latest Projects :</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
     {{-- </div> --}}
 @endsection
